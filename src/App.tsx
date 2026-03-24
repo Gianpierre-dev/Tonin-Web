68195eb899fc06cd8a988f0f695dbb60b3a4c48e
Gianpierre-dev <pier_terrazas@hotmail.com>
2026-03-24 09:26:34 -0500

    chore: configurar Tailwind CSS 4 con tema custom

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-function App() {
-  return <h1>Tonin</h1>
+const App = (): React.JSX.Element => {
+  return <div className="bg-warm-black text-text-dark min-h-screen flex items-center justify-center text-2xl">Tonin</div>
 }
-
 export default App
